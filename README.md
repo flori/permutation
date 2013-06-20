43804e8ead2bcc16e49ee6ba81a939fb07f8821a
Florian Frank <flori@ping.de>
2013-06-20 11:56:55 +0200

    Move to http://github.com/flori/more_math

---
+++ README.md
@@ -0,0 +1,3 @@
+# Permutation is now MoreMath::Permutation
+
+Moved to <http://github.com/flori/more_math>.
--- make_doc.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-$outdir = 'doc/'
-puts "Creating documentation in '#$outdir'."
-system "rdoc -d -m README -t 'Permutation library for Ruby' -o #$outdir #{(%w[README] + Dir['lib/**/*.rb']) * ' '}"
